ad313cfb6e4298c6c65f50e6d95112e9d6151e0c
Jose Casanova <josescasanova@gmail.com>
2014-03-18 09:06:04 -0400

    failing queue tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ActiveRecord::Base
   has_many :video_categories
   has_many :categories, through: :video_categories
   has_many :reviews, order: "created_at DESC"
-  
+
   validates :title, presence: true
   validates :description, presence: true
 
