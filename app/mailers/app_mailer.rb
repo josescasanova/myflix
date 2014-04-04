511c372e1fd7be62cb71ee117712a1a55a5f46c2
Jose Casanova <josescasanova@gmail.com>
2014-04-04 01:08:32 -0400

    changed from email to mailgun

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,16 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Welcome to Myflix!"
+    mail to: user.email, from: "info@app23722853.mailgun.org", subject: "Welcome to Myflix!"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Please reset your password"
+    mail to: user.email, from: "info@app23722853.mailgun.org", subject: "Please reset your password"
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail to: invitation.recipient_email, from: "info@myflix.com", subject: "Invitation to MyFlix"
+    mail to: invitation.recipient_email, from: "info@app23722853.mailgun.org", subject: "Invitation to MyFlix"
   end
 end
