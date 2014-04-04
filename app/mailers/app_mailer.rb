573c69f6f1fe0ed36b6fc09b29c8706ed1968510
Jose Casanova <josescasanova@gmail.com>
2014-04-04 18:08:45 -0400

    changed app_mailer to have staging variable

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,26 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, from: "info@app23722853.mailgun.org", subject: "Welcome to Myflix!"
+    mail to: staging_or_user_email, from: "info@app23722853.mailgun.org", subject: "Welcome to Myflix!"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "info@app23722853.mailgun.org", subject: "Please reset your password"
+    mail to: staging_or_user_email, from: "info@app23722853.mailgun.org", subject: "Please reset your password"
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail to: invitation.recipient_email, from: "info@app23722853.mailgun.org", subject: "Invitation to MyFlix"
+    mail to: staging_or_invitation_email, from: "info@app23722853.mailgun.org", subject: "Invitation to MyFlix"
+  end
+
+  private
+
+  def staging_or_user_email
+     Rails.env.staging? ? "josescasanova+staging@gmail.com" : "#{user.email}"
+  end
+
+  def staging_or_invitation_email
+    Rails.env.staging? ? "josescasanova+staging@gmail.com" : "#{invitation.recipient_email}"
   end
 end
