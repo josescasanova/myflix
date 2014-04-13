0be95cd4f913eeb70035a3e682b3d19ee74d84e6
Jose Casanova <josescasanova@gmail.com>
2014-04-13 00:11:36 -0400

    added decorator

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,7 +6,8 @@ class VideosController < ApplicationController
   end
 
   def show
-    @video = Video.find(params[:id])
+    @video = VideoDecorator.decorate(Video.find(params[:id]))
+    # @video = Video.find(params[:id])
     @reviews = @video.reviews
   end
 
