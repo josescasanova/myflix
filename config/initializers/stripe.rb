7086d2370831dfe0c8968f32b0aecafd4da03ede
Jose Casanova <josescasanova@gmail.com>
2014-04-17 00:55:01 -0400

    trying to fix route 404

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,14 @@
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
-StripeEvent.configure do |events|
-  events.subscribe 'charge.succeeded' do |event|
+# StripeEvent.configure do |events|
+#   events.subscribe 'charge.succeeded' do |event|
+#     user = User.where(customer_token: event.data.object.customer).first
+#     Payment.create(user: user, amount: event.data.object.amount, reference_id: event.data.object.invoice)
+#   end
+# end
+
+StripeEvent.configure do
+  StripeEvent.subscribe 'charge.succeeded' do |event|
     user = User.where(customer_token: event.data.object.customer).first
     Payment.create(user: user, amount: event.data.object.amount, reference_id: event.data.object.invoice)
   end
